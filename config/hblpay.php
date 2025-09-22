1addcf4657289e66a52400a48ccee5ff3c9ef617
webdev0320 <muhammadarbaz894@gmail.com>
2025-09-22 16:24:31 +0500

    Update hblpay.php

--- config/hblpay.php
+++ config/hblpay.php
@@ -1,7 +1,13 @@
 <?php
-
 return [
-    'merchant_id' => env('HBLPAY_MERCHANT_ID', ''),
-    'secret'      => env('HBLPAY_SECRET', ''),
-    'sandbox'     => env('HBLPAY_SANDBOX', true),
+    'env' => env('HBLPAYENV'), // 'production' for live
+    'user_id' => env('HBLUSERID'),
+    'password' => env('HBLPASSWORD'),
+    'channel' => env('HBLCHANNEL'),
+    'return_url' => env('HBLPAY_RETURN_URL'),
+    'cancel_url' => env('HBLPAY_CANCEL_URL'),
+    'rsa' => [
+        'public_key_path' => env('HBLPAY_PUBLIC_KEY_PATH'),
+        'private_key_path' => env('HBLPAY_PRIVATE_KEY_PATH'),
+    ],
 ];
